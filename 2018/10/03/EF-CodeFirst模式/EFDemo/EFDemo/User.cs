da0db2ee93e2ab708f1cc399b4f3d6239ff620d7
WxhShine <790875216@qq.com>
2018-11-03 15:07:02 +0800

    Site updated: 2018-11-03 15:06:58

---
+++ 2018/10/03/EF-CodeFirst模式/EFDemo/EFDemo/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFDemo {
+    public class User {
+        [Key,Index]//设置Id为主键,并添加索引
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ 2018/10/03/EF-CodeFirst模式/EFDemo/EFDemo/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace EFDemo {
+    public class UserContext :DbContext{
+        public UserContext() : base("EFDemoConnectionString") { }
+
+        public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
